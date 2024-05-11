512955367898825368c888f8fed637bfcdc249ff
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-05-11 15:32:35 -0400

    DEPLOY: Heroku

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,4 @@ const controller = require('../controllers/auth')
 router.post('/login', controller.Login)
 router.post('/register', controller.Register)
 
-module.exports = router
+module.exports = router 
