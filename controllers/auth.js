e42ad1cca614f43c91bf98db130a0a02504ef20c
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-05-14 17:59:33 -0400

    BE: login bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,25 +21,32 @@ const Register = async (req, res) => {
 };
 
 const Login = async (req, res) => {
-  console.log(req.body);
   try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    let matched = await middleware.comparePassword(user.password, password);
+    // Extracts the necessary fields from the request body
+    const { email, password } = req.body
+    // Finds a user by a particular field (in this case, email)
+    const user = await User.findOne({ email })
+    // Checks if the password matches the stored digest
+    let matched = await middleware.comparePassword(
+      user.passwordDigest,
+      password
+    )
+    // If they match, constructs a payload object of values we want on the front end
     if (matched) {
       let payload = {
         id: user.id,
-        email: user.email,
-      };
-      let token = middleware.createToken(payload);
-      return res.send({ user: payload, token });
+        email: user.email
+      }
+      // Creates our JWT and packages it with our payload to send as a response
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
     }
-    res.status(401).send({ status: "Error", msg: "Unauthorized" });
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
   } catch (error) {
-    console.log(error);
-    res.status(401).send({ status: "Error", msg: "An error has occurred!" });
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'An error has occurred!' })
   }
-};
+}
 
 const UpdatePassword = async (req, res) => {
   try {
