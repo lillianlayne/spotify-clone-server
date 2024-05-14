4a0596623e823126a0c25eb43f4027bc7681d45e
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-05-14 15:34:39 -0400

    FEATURE: add song to playlist

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,6 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS);
 const APP_SECRET = process.env.APP_SECRET;
@@ -8,53 +8,51 @@ const APP_SECRET = process.env.APP_SECRET;
 const hashPassword = async (password) => {
   let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
   return hashedPassword;
-}
+};
 
 const comparePassword = async (storedPassword, password) => {
   let passwordMatch = await bcrypt.compare(password, storedPassword);
-  return passwordMatch
-}
+  return passwordMatch;
+};
 
 const createToken = (payload) => {
   let token = jwt.sign(payload, APP_SECRET);
   return token;
-}
+};
+
+const stripToken = (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].split(" ")[1];
+    if (token) {
+      res.locals.token = token;
+      return next();
+    }
+    res.status(401).send({ status: "Error", msg: "Unauthorized" });
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({ status: "Error", msg: "Strip Token Error!" });
+  }
+};
 
 const verifyToken = (req, res, next) => {
+  const { token } = res.locals;
   try {
     let payload = jwt.verify(token, APP_SECRET);
     if (payload) {
       res.locals.payload = payload;
-      return next()
+      return next();
     }
-    res.status(401).send({ status: 'Error', msg: 'Unauthroized'})
+    res.status(401).send({ status: "Error", msg: "Unauthorized" });
   } catch (error) {
-    console.log(error)
-    res.status(401).send({ status: 'Error', msg: 'Verify Token Error!' })
+    console.log(error);
+    res.status(401).send({ status: "Error", msg: "Verify Token Error!" });
   }
-}
-
-const stripToken = (req, res, next) => {
-  try {
-    const token = req.headers['authorization'].split(' ')[1]
-    // Gets the token from the request headers {authorization: Bearer Some-Token}
-    // Splits the value of the authorization header
-    if (token) {
-      res.locals.token = token
-      // If the token exists we add it to the request lifecycle state
-      return next()
-    }
-    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
-  } catch (error) {
-    console.log(error)
-    res.status(401).send({ status: 'Error', msg: 'Strip Token Error!' })
-  }
-}
+};
 
 module.exports = {
+  hashPassword,
+  comparePassword,
+  createToken,
   stripToken,
   verifyToken,
-  createToken,
-  comparePassword,
-  hashPassword
-}
+};
