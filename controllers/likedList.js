e2a49b4cdd0c2cbe124f29cd4540f7045717ef02
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-05-17 16:24:26 -0400

    FEAT: delete songs and artist

--- controllers/likedList.js
+++ controllers/likedList.js
@@ -1,88 +1,112 @@
-const express = require('express');
-const {User} = require('../models');
+const express = require("express");
+const { User } = require("../models");
 
 const addSong = async (req, res) => {
- try {
-  const user = await User.findById(req.params.id);
-  if (!user) {
-    return res.status(404).json({error: "user not found"})
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: "user not found" });
+    }
+
+    const data = req.body;
+    user.likedSongs.push(data);
+
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(400).json(error);
   }
-
-  const data = req.body;
-  user.likedSongs.push(data)
-
-  await user.save()
-  res.json(user)
- } catch (error) {
-  res.status(400).json(error)
- }
-}
+};
 
 const addAlbum = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     if (!user) {
-      return res.status(404).json({error: "user not found"})
+      return res.status(404).json({ error: "user not found" });
     }
-  
+
     const data = req.body;
-    user.likedAlbums.push(data)
-  
-    await user.save()
-    res.json(user)
-   } catch (error) {
-    res.status(400).json(error)
-   }
-}
+    user.likedAlbums.push(data);
+
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 
 const addArtist = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     if (!user) {
-      return res.status(404).json({error: "user not found"})
+      return res.status(404).json({ error: "user not found" });
     }
-  
+
     const data = req.body;
-    user.likedArtists.push(data)
-  
-    await user.save()
-    res.json(user)
-   } catch (error) {
-    res.status(400).json(error)
-   }
-}
+    user.likedArtists.push(data);
 
-const deleteSong = async (req, res) => {
-  const user = await User.findById(req.params.id)
-  const songs = user.likedSongs;
-
-  try {
-  res.json(await songs.findByIdAndDelete(req.body))
+    await user.save();
+    res.json(user);
   } catch (error) {
-    res.status(400).json(error)
+    res.status(400).json(error);
   }
-}
+};
 
 const deleteAlbum = async (req, res) => {
-  const user = await User.findById(req.params.id)
-  const albums = user.likedAlbums;
-  const albumToRemove = req.body.content
+  const user = await User.findById(req.params.id);
+  const itemToRemove = req.body.content;
 
   try {
-    const indexRemove = user.likedAlbums.findIndex((album) => album.content === albumToRemove);
-    user.likedAlbums.splice(indexRemove, 1)
-    await user.save()
-    res.json(user)
-
+    const indexRemove = user.likedAlbums.findIndex(
+      (item) => item.content === itemToRemove
+    );
+    user.likedAlbums.splice(indexRemove, 1);
+    await user.save();
+    res.json(user);
   } catch (error) {
-    res.status(400).json(error)
+    res.status(400).json(error);
   }
-  
-}
+};
+
+const deleteSong = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  const itemToRemove = req.body.content;
+
+  try {
+    const indexRemove = user.likedSongs.findIndex(
+      (item) => item.content === itemToRemove
+    );
+    user.likedSongs.splice(indexRemove, 1);
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const deleteArtist = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  const itemToRemove = req.body.content;
+
+  try {
+    const indexRemove = user.likedArtists.findIndex(
+      (item) => item.content === itemToRemove
+    );
+    user.likedArtists.splice(indexRemove, 1);
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+
 
 module.exports = {
-  addSong, 
+  addSong,
   addAlbum,
-  addArtist, 
-  deleteAlbum
-}
+  addArtist,
+  deleteAlbum,
+  deleteArtist, 
+  deleteSong
+};
--- routes/users.js
+++ routes/users.js
@@ -22,6 +22,8 @@ router.post("/:id/songs", likedCtrl.addSong);
 router.post("/:id/albums", likedCtrl.addAlbum);
 router.post("/:id/artists", likedCtrl.addArtist);
 router.delete("/:id/albums", likedCtrl.deleteAlbum);
+router.delete("/:id/songs", likedCtrl.deleteSong);
+router.delete("/:id/artists", likedCtrl.deleteArtist);
 
 
 module.exports = router
